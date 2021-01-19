f85c2ca633d184b5da540ade278901d1fceb07f7
m0zart89 <mozart-freelance@yandex.ru>
2021-01-19 11:15:31 +0200

    Update openstack_orphaned_resource.py

--- openstack_orphaned_resource.py
+++ openstack_orphaned_resource.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python
-"""openstack_orphaned_resource.py: This script will list the resources that are allocated to non-existing Project IDs."""
-__author__      = "Vinoth Kumar Selvaraj"
-
-import os
-import sys
-from keystoneauth1 import identity
-from keystoneauth1 import session
-from keystoneclient.v3 import client
-from neutronclient.v2_0 import client as nclient
-from novaclient import client as novaclient
-
-def usage():
-    print "listorphans.py <object> where object is one or more of",
-    print "'networks', 'routers', 'subnets', 'floatingips', 'ports', 'servers', 'secgroup' or 'all'"
+import os, sys, json
+from termcolor import colored, cprint
+from datetime import datetime
+from keystoneauth1.identity import v2
+from keystoneauth1 import session as ka_session
+from keystoneclient.v3 import client as kclient
+from novaclient import client as nclient
+from neutronclient.v2_0 import client as neuclient
+from cinderclient import client as cclient
+from glanceclient.v2 import client as glclient
 
 username=os.environ['OS_USERNAME']
 password=os.environ['OS_PASSWORD']
-project_name=os.environ['OS_PROJECT_NAME']
-project_domain_id='default'
-user_domain_id='default'
 auth_url=os.environ['OS_AUTH_URL']
-auth = identity.Password(auth_url=auth_url, username=username, password=password, project_name=project_name, project_domain_id=project_domain_id, user_domain_id=user_domain_id)
+region=os.environ['OS_REGION_NAME']
 
-sess = session.Session(auth=auth)
-keystone = client.Client(session=sess)
-neutron = nclient.Client(session=sess)
-nova = novaclient.Client(2.0, session=sess)
+def usage():
+    print "openstack_orphaned_resource.py <object> where object is one or more of",
+    print "'networks', 'routers', 'subnets', 'floatingips', 'ports', 'servers', 'volumes', 'images' or 'all'"
 
-def get_projectids():
-    return [project.id for project in keystone.projects.list()]
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+def print_json(data):
+    print(json.dumps(data, sort_keys=True, indent=7, separators=(',', ':'), default=myconverter))
+
+def get_session():
+    auth = v2.Password(auth_url=auth_url, username=username,
+                       password=password, tenant_name="admin")
+    session = ka_session.Session(auth=auth)
+    return session
+
 
 def get_orphaned_neutron_objects(object):
     projectids = get_projectids()
@@ -58,24 +60,88 @@ def get_orphaned_security_group_objects():
            orphans.append(secgroup.id)
     return orphans
 
+def get_orphaned_volumes_objects():
+    projectids = get_projectids()
+    projectids.append("")
+    orphans = []
+    for volume in cinder.volumes.list(search_opts={'all_tenants': 1}):
+        if getattr(volume, 'os-vol-tenant-attr:tenant_id') not in projectids:
+           orphans.append(volume.id)
+    return orphans
+
+def get_orphaned_images_objects():
+    projectids = get_projectids()
+    projectids.append("")
+    orphans = []
+    for image in glance.images.list():
+        if image.owner not in projectids:
+           orphans.append(image.owner)
+    return orphans
+
+def get_orphaned_security_group_objects():
+    projectids = get_projectids()
+    projectids.append("")
+    orphans = []
+    for secgroup in nova.security_groups.list(search_opts={'all_tenants': 1}):
+        if secgroup.tenant_id not in projectids:
+           orphans.append(secgroup.id)
+    return orphans
+
+def get_orphaned_keypairs_objects():
+    projectids = get_projectids()
+    projectids.append("")
+    orphans = []
+    print nova.keypairs.list()
+    for keypair in nova.keypairs.list():
+        print keypair
+        sys.exit(0)
+        if keypair.tenant_id not in projectids:
+           orphans.append(secgroup.id)
+    return orphans
+
+session=get_session()
+keystone = kclient.Client(session=session)
+neutron = neuclient.Client(session=session, region_name=region)
+nova = nclient.Client(2, session=session, region_name=region)
+cinder = cclient.Client('2', session=session, region_name=region)
+glance = glclient.Client('2', session=session, region_name=region)
+
+projectids = [project.id for project in keystone.projects.list()]
+
+def get_projectids():
+    return projectids
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'all':
-            objects = [ 'networks', 'routers', 'subnets', 'floatingips', 'ports', 'servers', 'secgroup' ]
+            objects = [ 'networks', 'routers', 'subnets', 'floatingips', 'ports', 'servers', 'volumes', 'images' ]
         else:
             objects = sys.argv[1:]
         for object in objects:
             if object=="servers":
               orphans = get_orphaned_nova_objects()
-              print len(orphans), 'orphan(s) found of type', object
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
               print '\n'.join(map(str, orphans))
-            elif object=="secgroup":
+            elif object=="keypairs":
+              orphans = get_orphaned_keypairs_objects()
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
+              print '\n'.join(map(str, orphans))
+            elif object=="volumes":
+              orphans = get_orphaned_volumes_objects()
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
+              print '\n'.join(map(str, orphans))
+            elif object=="secgroups":
               orphans = get_orphaned_security_group_objects()
-              print len(orphans), 'orphan(s) found of type', object
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
+              print '\n'.join(map(str, orphans))
+            elif object=="images":
+              orphans = get_orphaned_images_objects()
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
               print '\n'.join(map(str, orphans))
             else:
               orphans = get_orphaned_neutron_objects(object)
-              print len(orphans), 'orphan(s) found of type', object
+              print len(orphans), 'orphan(s) found of type', colored(object, 'green')
               print '\n'.join(map(str, orphans))
     else:
         usage()
